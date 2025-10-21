338659d4f68d82f4cd3f3afc0dd298ba1cc69f91
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2025-10-21 21:59:31 +0700

    fix component sortable

--- src/features/exercises/components/ExerciseFormDialog.tsx
+++ src/features/exercises/components/ExerciseFormDialog.tsx
@@ -1,43 +1,43 @@
-'use client';
+// 'use client';
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
-import { ReactNode, useState } from 'react';
-import { ExerciseForm } from './ExerciseForm';
+// import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+// import { ReactNode, useState } from 'react';
+// import { ExerciseForm } from './ExerciseForm';
 
-export function ExerciseFormDialog({
-    classroomId,
-    exercise,
-    children,
-}: {
-    classroomId: string;
-    exercise?: {
-        id: string;
-        name: string;
-        description: string;
-        subjectName: string;
-        dueDate?: Date;
-        maxScore?: number;
-    };
-    children: ReactNode;
-}) {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <Dialog open={isOpen} onOpenChange={setIsOpen}>
-            {children}
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle className="cursor-pointer">
-                        {exercise == null ? 'New exercise' : `Edit ${exercise.name}`}
-                    </DialogTitle>
-                </DialogHeader>
-                <div className="mt-4 ">
-                    <ExerciseForm
-                        exercise={exercise}
-                        classroomId={classroomId}
-                        onSucess={() => setIsOpen(false)}
-                    ></ExerciseForm>
-                </div>
-            </DialogContent>
-        </Dialog>
-    );
-}
+// export function ExerciseFormDialog({
+//     classroomId,
+//     exercise,
+//     children,
+// }: {
+//     classroomId: string;
+//     exercise?: {
+//         id: string;
+//         name: string;
+//         description: string;
+//         subjectName: string;
+//         dueDate?: Date;
+//         maxScore?: number;
+//     };
+//     children: ReactNode;
+// }) {
+//     const [isOpen, setIsOpen] = useState(false);
+//     return (
+//         <Dialog open={isOpen} onOpenChange={setIsOpen}>
+//             {children}
+//             <DialogContent>
+//                 <DialogHeader>
+//                     <DialogTitle className="cursor-pointer">
+//                         {exercise == null ? 'New exercise' : `Edit ${exercise.name}`}
+//                     </DialogTitle>
+//                 </DialogHeader>
+//                 <div className="mt-4 ">
+//                     <ExerciseForm
+//                         exercise={exercise}
+//                         classroomId={classroomId}
+//                         onSucess={() => setIsOpen(false)}
+//                     ></ExerciseForm>
+//                 </div>
+//             </DialogContent>
+//         </Dialog>
+//     );
+// }
--- src/features/exercises/components/SortableExerciseList.tsx
+++ src/features/exercises/components/SortableExerciseList.tsx
@@ -1,65 +1,65 @@
-'use client';
+// 'use client';
 
-import { SortableItem, SortableList } from '@/components/SortableList';
-import { Trash2Icon } from 'lucide-react';
-import { DialogTrigger } from '@/components/ui/dialog';
-import { Button } from '@/components/ui/button';
-import { ActionButton } from '@/components/ActionButton';
-import { deleteExerciseAction, updateExerciseOrdersAction } from '../actions/exercises';
-import { ExerciseFormDialog } from './ExerciseFormDialog';
+// import { SortableItem, SortableList } from '@/components/SortableList';
+// import { Trash2Icon } from 'lucide-react';
+// import { DialogTrigger } from '@/components/ui/dialog';
+// import { Button } from '@/components/ui/button';
+// import { ActionButton } from '@/components/ActionButton';
+// import { deleteExerciseAction, updateExerciseOrdersAction } from '../actions/exercises';
+// import { ExerciseFormDialog } from './ExerciseFormDialog';
 
-export function SortableExerciseList({
-    classroomId,
-    exercises,
-}: {
-    classroomId: string;
-    exercises: {
-        id: string;
-        name: string;
-        description: string;
-        subjectName: string;
-        dueDate?: Date;
-        maxScore?: number;
-        order?: number;
-    }[];
-}) {
-    return (
-        <SortableList
-            items={exercises}
-            onOrderChange={(newOrder) => updateExerciseOrdersAction(classroomId, newOrder)}
-        >
-            {(items) =>
-                items.map((exercise) => (
-                    <SortableItem
-                        key={exercise.id}
-                        id={exercise.id}
-                        className="flex items-center gap-1"
-                    >
-                        <div>{exercise.name}</div>
-                        <ExerciseFormDialog exercise={exercise} classroomId={classroomId}>
-                            <DialogTrigger asChild>
-                                <Button
-                                    variant="outline"
-                                    size="sm"
-                                    className="ml-auto cursor-pointer"
-                                >
-                                    Edit
-                                </Button>
-                            </DialogTrigger>
-                        </ExerciseFormDialog>
-                        <ActionButton
-                            action={deleteExerciseAction.bind(null, exercise.id)}
-                            requireAreYouSure
-                            variant="destructive"
-                            size="sm"
-                            className="hover:text-white cursor-pointer"
-                        >
-                            <Trash2Icon className="cursor-pointer hover:text-black" />
-                            <span className="sr-only ">Delete</span>
-                        </ActionButton>
-                    </SortableItem>
-                ))
-            }
-        </SortableList>
-    );
-}
+// export function SortableExerciseList({
+//     classroomId,
+//     exercises,
+// }: {
+//     classroomId: string;
+//     exercises: {
+//         id: string;
+//         name: string;
+//         description: string;
+//         subjectName: string;
+//         dueDate?: Date;
+//         maxScore?: number;
+//         order?: number;
+//     }[];
+// }) {
+//     return (
+//         <SortableList
+//             items={exercises}
+//             onOrderChange={(newOrder) => updateExerciseOrdersAction(classroomId, newOrder)}
+//         >
+//             {(items) =>
+//                 items.map((exercise) => (
+//                     <SortableItem
+//                         key={exercise.id}
+//                         id={exercise.id}
+//                         className="flex items-center gap-1"
+//                     >
+//                         <div>{exercise.name}</div>
+//                         <ExerciseFormDialog exercise={exercise} classroomId={classroomId}>
+//                             <DialogTrigger asChild>
+//                                 <Button
+//                                     variant="outline"
+//                                     size="sm"
+//                                     className="ml-auto cursor-pointer"
+//                                 >
+//                                     Edit
+//                                 </Button>
+//                             </DialogTrigger>
+//                         </ExerciseFormDialog>
+//                         <ActionButton
+//                             action={deleteExerciseAction.bind(null, exercise.id)}
+//                             requireAreYouSure
+//                             variant="destructive"
+//                             size="sm"
+//                             className="hover:text-white cursor-pointer"
+//                         >
+//                             <Trash2Icon className="cursor-pointer hover:text-black" />
+//                             <span className="sr-only ">Delete</span>
+//                         </ActionButton>
+//                     </SortableItem>
+//                 ))
+//             }
+//         </SortableList>
+//     );
+// }
